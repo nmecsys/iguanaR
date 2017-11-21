007e4fe58f5495da1a9408d654bf29940a91ca02
Jonatha Azevedo <johnmasterchip@gmail.com>
2017-11-21 18:04:20 -0200

    up

--- R/iguana.reader.R
+++ R/iguana.reader.R
@@ -23,20 +23,28 @@ iguana.reader<-function(token,cenario = c(1,2,3),start,end){
 
   ##jornais
   ##- valor online
-  valor_online = iguana.get(token=token,datainicio = start,datafim = end,fonte = "Valor_Economico")
+  valor_online = iguana.get(token=token,datainicio = start,datafim = end,fonte = "Valor_Economico")$noticias
+  leitor_valor_online = leitor(valor_online,start_date = start,end_date=end)
   ##- valor impresso
   valor_impresso = iguana.get(token=token,datainicio = start,datafim = end,fonte = "Valor_impresso")$noticias
+  leitor_valor_impresso = leitor(valor_impresso,start_date = start,end_date=end)
   ##- folha online
   folha_online = iguana.get(token=token,datainicio = start,datafim = end,fonte = "Folha_online")$noticias
+  leitor_folha_online = leitor(folha_online,start_date = start,end_date=end)
   ##- folha impresso
   folha_impresso = iguana.get(token=token,datainicio = start,datafim = end,fonte = "Folha_impresso")$noticias
+  leitor_folha_impresso = leitor(folha_impresso,start_date = start,end_date=end)
   ##- @estadao
   estadao  = iguana.get(token=token,datainicio = start,datafim = end,fonte = "@estadao")$noticias
+  leitor_estadao = leitor(estadao,start_date = start,end_date=end)
   ##- @correio
   correio  = iguana.get(token=token,datainicio = start,datafim = end,fonte = "@correio")$noticias
+  leitor_correio = leitor(correio,start_date = start,end_date=end)
   ##- @oglobo
   oglobo   = iguana.get(token=token,datainicio = start,datafim = end,fonte = "@oglobo")$noticias
+  leitor_oglobo = leitor(oglobo,start_date = start,end_date=end)    
   ##- @zerohora
   zerohora = iguana.get(token=token,datainicio = start,datafim = end,fonte = "zehora")$noticias
+  leitor_zerohora = leitor(zerohora,start_date = start,end_date=end)
 
 }
